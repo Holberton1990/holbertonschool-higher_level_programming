6934811fdd554caeff464bcd74bde301d7e0988c
Holberton1990 <ersida9019@gmail.com>
2023-09-27 11:01:18 +0000

    print alphabety

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python3.
+#!/usr/bin/python3
+
 for alpha_letters in range(ord('a'), ord('z')+1):
-    if alpha_letters == 'e' or alpha_letters == 'q':
-       continue
-    print(f"{:c}".format(alpha_letters), end="")
+    if alpha_letters == ord('e') or alpha_letters == ord('q'):
+        continue
+    print(f"{chr(alpha_letters)}", end="")
