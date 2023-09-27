ca7e8837e20ee75b5d3b01f683f9f17b01a22d3e
Holberton1990 <ersida9019@gmail.com>
2023-09-27 10:55:09 +0000

    print alphabety

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for alpha_letters in range(ord('a'), ord('z')+1):
     if alpha_letters == 'e' or alpha_letters == 'q':
        continue
-    print("{:c}".format(alpha_letters), end="")
+    print(f"{:c}".format(alpha_letters), end="")
