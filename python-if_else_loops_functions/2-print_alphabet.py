27318d34b60d39a1884e914ae9966b59ffdc26ef
Holberton1990 <ersida9019@gmail.com>
2023-09-27 10:33:12 +0000

    last digit

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3 
-for ascii_value in range(ord('a'), ord('z')+1):
-    print(f"{chr(ascii_value)}", end=' ')
+#!/usr/bin/python3
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
