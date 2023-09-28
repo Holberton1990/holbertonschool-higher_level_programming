2319f4da57aa30b3f5193b7bdce1e5569c8cb8cd
Holberton1990 <ersida9019@gmail.com>
2023-09-28 21:20:50 +0000

    hidden_discovery

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-inport hidden_4.pyc
-    name = dir(hidden.4)
-    for name in name:
-        if name[0] != 0:
-        print(name)
+if __name__ == "__main__":
+    import hidden_4
+    names = dir(hidden_4)
+    for name in names:
+        if name[0] != "_":
+            print(name)
+            
