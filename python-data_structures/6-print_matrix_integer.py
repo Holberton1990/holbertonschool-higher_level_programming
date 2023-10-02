656fe0233ecd0bf7c8a4fb8071d92a572cbe899b
Holberton1990 <ersida9019@gmail.com>
2023-10-02 20:18:47 +0000

    new-list

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix():
+        for columns in row:
+            print("{:d}".format(columns), end="")
+            if row.index(columns) != len(row) - 1:
+                print(" ".format(), end="")
+        print()
