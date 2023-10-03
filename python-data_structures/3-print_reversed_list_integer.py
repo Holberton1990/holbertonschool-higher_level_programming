150009c9553c01ce7fc5dc2f4115c001034489bc
Holberton1990 <ersida9019@gmail.com>
2023-10-03 11:10:14 +0000

    addtuple

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     if my_list is not None:
         rev_list = my_list[::-1]
         for num in rev_list:
-        print("{:d}".format(num))
+            print("{:d}".format(num))
