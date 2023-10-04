e3b58238642bcb9f5ae3e5b9d27815f883d227d7
Holberton1990 <ersida9019@gmail.com>
2023-10-04 18:15:16 +0000

    insection

---
+++ python-more_data_structures/ushtrimet.py
@@ -0,0 +1,38 @@
+# !/usr/bin/python3
+# # set1 = {1, 2, 3}
+# # set2 = {3, 4, 5}
+# # union_set = set1 | set2
+# # union_set = {1,2,3,4,5}
+# # intersection_set = set1 & set2
+# # intersection_set = {3}
+# # difference_set = set1 - set2
+# # difference_set = {1, 2}
+# # Symmetruc_Difference = set1 ^ set2
+# # Symmetruc_Difference = {1, 2,4,5}
+# my_dict = {"name": "Alice", "age": 25, "city": "New York"}
+# another_dict = dict(name = "Bob", age = 30, city = "Los Angeles")
+# name = my_dict["name"]
+# print(name)
+# my_dict[0] = ""
+# my_dict
+# # Adding and Updato=ing Entries
+# # Remove
+# del my_dict["city"]
+# my_dict
+# my_dict.pop("age")
+# # Checking for Key Existence
+# for key in my_dict:
+#     print(key, my_dict[key])
+#     # getting Keys and Values 
+#     key_list = list(my dict.keys())
+#     print key_list
+#     value_list = list(my_dict.values)
+#  def remove_dublicates(imput_list)
+#      unique_list = []:
+#      for x in input_list:
+#         if x == y:
+#             unique_list.Remove(x)
+#             return unique_list
+#         print remove_dublicates
+#         unique_list = list(set(imput_list))
+#         return unique_list
