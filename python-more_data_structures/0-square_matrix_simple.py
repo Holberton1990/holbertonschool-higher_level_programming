4fb37231772ae401039c008b85002b13745983e4
Holberton1990 <ersida9019@gmail.com>
2023-10-04 10:01:08 +0000

    matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    square = lambda x: x ** 2
-    squared_matrix = list(map(lambda row: list(map(square, row)), matrix))
-    return squared_matrix
- 
+    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
---
+++ python-more_data_structures/ushtrime.py
@@ -0,0 +1,25 @@
+element,for,kushti,
+palindromat
+element,loop,kushti->kjo eshte list komperejshen
+sete(te parenditur dhe unik)keto duhen per union i dy bashkesive {intersection(element te perfshijn te dyja and llogjik)}
+'''''
+my set = {1,2,3,4}
+shton ne metoden set,heq me remove
+per te kontrolluar
+if 5 in my set:
+    print("5 is in the set")
+    unino(|)
+    hash map indeksohet me ane te key,key eeeshte dicka unike
+enumerate-interrimi i dufisht jep dhe pozicion dhe vler ne nje list.items merr dhe key
+ ''''
+def frecuency_counter
+    my_dict = {}
+    words = text.split()
+    print (words)
+    for word in words:
+        if word not in my_dict
+            my_dict1[word] = 1
+        else:
+            my_dict1[word] +=1
+    return my_dict
+print(frecuency_counter("I'm the best"))
