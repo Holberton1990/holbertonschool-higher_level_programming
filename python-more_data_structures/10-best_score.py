70cb039788118fd316715a85855ff4c971dd8b8a
Holberton1990 <ersida9019@gmail.com>
2023-10-05 19:26:04 +0000

    0-safe

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a a_dictionary is not None:
+    if a_dictionary is not None:
         max_val = max(a_dictionary, key=a_dictionary.get, default=None)
-        return max_val
+        return (max_val)
     else:
-        return None
+        return (None)
