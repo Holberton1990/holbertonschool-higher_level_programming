90e347eaf384f60474a38ec2e26c52fabfddd8ab
Holberton1990 <ersida9019@gmail.com>
2023-10-10 09:54:03 +0000

    square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -2,7 +2,9 @@
 """
 python3 -c 'print(__import__("my_module").__doc__)'
 python3 -c 'print(__import__("my_module").MyClass.__doc__)'
-""" 
+"""
+
+
 class Square:
     """
     This is the documentation for the Square class.
@@ -40,4 +42,4 @@ class Square:
         python3 -c 'print(__import__("my_module").my_function.__doc__)'
         python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
         """
-        return self.__size
+        return self.__size ** 2
