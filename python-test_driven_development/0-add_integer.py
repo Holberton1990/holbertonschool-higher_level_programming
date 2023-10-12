6abb7bccb438c15d3c7fce4dab6d2bd3e01d3f32
Holberton1990 <ersida9019@gmail.com>
2023-10-12 14:28:03 +0000

    add

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,10 +2,12 @@
 """
 python3 -c 'print(__import__("my_module").__doc__)'
 """
+
+
 def add_integer(a, b=98):
     """
-    python3 -c 'print(__import__("my_module").__doc__)'
-    """ 
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
