6fae7b164cfdcf5587e9c4a5ea6486562ea60f29
Holberton1990 <ersida9019@gmail.com>
2023-10-12 15:05:23 +0000

    add

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
 def print_square(size):
-    size = len(square)
+    """
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
