76312771d514a7d192b945d0b0f8287949d17e6e
Holberton1990 <ersida9019@gmail.com>
2023-10-18 09:53:01 +0000

    lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,9 +2,10 @@
 """
 python3 -c 'print(__import__("my_module").__doc__)'
 """
+
 def lookup(obj):
      """
     'print(__import__("my_module").my_function.__doc__)'
     python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
     """
-    return dir(obj)
+    return (dir(obj))
