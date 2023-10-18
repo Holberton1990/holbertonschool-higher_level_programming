75f153e257045aa2f2acb38f3cc7c468d5e0746c
Holberton1990 <ersida9019@gmail.com>
2023-10-18 13:25:32 +0000

    mylist

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -9,7 +9,7 @@ def inherits_from(obj, a_class):
     'print(__import__("my_module").my_function.__doc__)'
     python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
     """
-    if isinstance(obj, a_class):
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
 
