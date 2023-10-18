2be223294f305dfefd92abb7e4002cdd587d4289
Holberton1990 <ersida9019@gmail.com>
2023-10-18 13:55:40 +0000

    mylist

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,13 +3,16 @@
 python3 -c 'print(__import__("my_module").__doc__)'
 """
 
-class MyList(list):
 
-def print_sorted(self):
+class MyList(list):
     """
-    'print(__import__("my_module").my_function.__doc__)'
-    python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
     """
-    sorted_list = sorted(self)
-    print (sorted_list(self))
-    return sorted_list
+    def print_sorted(self):
+        """
+        'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        sorted_list = sorted(self)
+        print(sorted(self))
+        return sorted_list
