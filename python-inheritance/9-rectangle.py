968701857bc68e3075335a9d870e258b0e2b1635
Holberton1990 <ersida9019@gmail.com>
2023-10-18 20:23:27 +0000

    mylist

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -18,3 +18,9 @@ class Rectangle(BaseGeometry):
         BaseGeometry.integer_validator(self, "height", height)
         self.__width = width
         self.__height = height
+    
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
