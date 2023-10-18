10ceb6bdf1a9364f0f2c25d130f3abab896f7449
Holberton1990 <ersida9019@gmail.com>
2023-10-18 13:14:41 +0000

    mylist

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    'print(__import__("my_module").my_function.__doc__)'
+    python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
+
+
+    
+"""
+return isinstance(obj, a_class)
+"""
