99960505268af8cfad2186fc7ea7d1c3620991da
Holberton1990 <ersida9019@gmail.com>
2023-10-18 13:30:53 +0000

    mylist

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -4,7 +4,7 @@ python3 -c 'print(__import__("my_module").__doc__)'
 """
 
 
-class BaseGeometry
+class BaseGeometry:
     """
     'print(__import__("my_module").my_function.__doc__)'
     python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
