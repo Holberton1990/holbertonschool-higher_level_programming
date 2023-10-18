61aa7b0d8669fe353d7592f521851c3684787edf
Holberton1990 <ersida9019@gmail.com>
2023-10-18 19:24:27 +0000

    mylist

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-"""
-python3 -c 'print(__import__("my_module").__doc__)'
-"""
+
+'''
+Python simple module
+for demonstarative purpose
+'''
 
 
-class BaseGeometry:
-   """
-python3 -c 'print(__import__("my_module").__doc__)'
-"""
-def area(self):
+class BaseGeometry():
     '''
+    Class that inherits from list class
+    '''
+    def area(self):
+        '''
         Instance method to raise an Exception
         '''
-    raise Exception("area() is not implemented")
-def integer_validator(self, name, value):
-    '''
-    Fubction for checking an valid integer
-    '''
-    if type(value) is not int:
-        raise TypeError(f"{name} must be an integer")
-    if type(value) <= 0:
-        raise TypeError(f"{name} must be grater than 0")
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''
+        Function for checking an valid integer
+        '''
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
