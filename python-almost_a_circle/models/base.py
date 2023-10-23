67f10269fd357462f8c88d3a78f3dd77cb782ef2
Holberton1990 <ersida9019@gmail.com>
2023-10-23 21:53:17 +0000

    python-almost

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+class Base:
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base._nb_objects += 1
+            self.id = Base._nb_objects
+ 
+ class Rectangle():
+
+    def area(self):
+        return self.__height * self__width
---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+from models.base import Base
+
+class Rectangle(Base):
+    """
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        self.__width = width
+        self.__height = height
+        self.__x = x 
+        self.__y = y
+        super().__init__(id)
+
+@property
+def width(self):
+    return self._width
+@width.setter
+def width(self, value):
+    if width is not integer:
+        raise TypeError(" width must be an integer")
+    if width < 0:
+        return ValueError("width must be >= 0")
+self.__width = width
+
+@property
+def height(self):
+    return self.__height
+@height.setter
+def height(self, value):
+     if type(value) != int:
+            raise TypeError("height must be an integer")
+    if height < 0:
+        return ValueError("height must be >= 0")
+    self.__height = height
+
+@property
+def x(self):
+    return self.__x
+@width.setter
+def x(self, value):
+     if type(value) != int:
+            raise TypeError("x must be an integer")
+    if x < 0:
+        return ValueError("x must be >= 0")
+    self.__x = x 
+
+@property
+def y(self):
+    return self._y
+@y.setter
+def y(self, value):
+    if type(value) != int:
+            raise TypeError("y must be an integer")
+    if y < 0:
+        return ValueError("y must be >= 0")
+    self.__y = y
