950551aa243bf01a9b10d2c3272a18baf77f7fbb
Holberton1990 <ersida9019@gmail.com>
2023-10-26 10:59:42 +0000

    base

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -44,3 +44,22 @@ class Square(Rectangle):
             "x": self.x,
             "y": self.y
         }
+
+        
+    @classmethod
+    def load_from_file(cls):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        file = cls.__name__ + ".json"
+        l_inst = []
+        try:
+            with open(file, "r") as f:
+                l_dict = Base.from_json_string(f.read())
+                for d_item in l_dict:
+                    inst = cls.create(**d_item)
+                    l_inst.append(inst)
+            return l_inst
+        except FileNotFoundError:
+            return []
