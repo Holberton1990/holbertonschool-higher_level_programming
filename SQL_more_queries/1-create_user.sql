036292d78498e0544c7c4ee59765ab13fb17ca22
Holberton1990 <ersida9019@gmail.com>
2023-11-23 03:39:54 -0800

    myprivileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-DROP USER IF EXIST 'user_od_1'@'localhost'
 CREATE USER 'user_0d_1'localhost' IDENTIFIED BY' user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'localhost' WITH GRANT OPTION;
