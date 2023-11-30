552239d15e70cd5e41984e756e93160a7675cbcf
Holberton1990 <ersida9019@gmail.com>
2023-11-30 03:21:49 -0800

    AirBnB

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument
+List all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
 """
 
 
@@ -18,8 +18,9 @@ if __name__ == "__main__":
             port=3306
             )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
-            ORDER BY states.id ASC".format(sys.argv[4]))
+    cur.execute(
+            "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+            )
     rows = cur.fetchall()
     for row in rows:
         print(row)
